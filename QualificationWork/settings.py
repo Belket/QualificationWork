61feefa27b39dd696d96c5648256cee5f0c1d37f
Gleb <glebchek@rambler.ru>
2019-05-24 01:22:48 +0300

    Adding contacts

--- QualificationWork/settings.py
+++ QualificationWork/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'HandBook',
     'ElementOffer',
     'Elements',
+    'Contact'
 ]
 
 MIDDLEWARE = [
--- QualificationWork/urls.py
+++ QualificationWork/urls.py
@@ -7,6 +7,7 @@ from HomePage import views as home_views
 from PersonalAccount import views as personal_account_views
 from HandBook import views as handbook_views
 from ElementOffer import views as offer_views
+from Contact import  views as contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,6 +17,7 @@ urlpatterns = [
     path('offer_element/', offer_views.offer_element),
     path('admin/offer/confirm/', offer_views.confirm_offer),
     path('admin/offer/reject/', offer_views.reject_offer),
+    path('contact/', contact.contact_us),
     path('collect_for_table/', offer_views.collect_for_table),
     path('personal_account/', personal_account_views.show_account),
     path('personal_account/change_data/', personal_account_views.change_data),
