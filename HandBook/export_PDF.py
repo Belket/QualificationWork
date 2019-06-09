5975daac63f28f1d7d10411207197a82332a6e8a
Gleb <glebchek@rambler.ru>
2019-06-10 02:25:02 +0300

    Fixes

--- HandBook/export_PDF.py
+++ HandBook/export_PDF.py
@@ -1,21 +1,27 @@
-from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus import *
 from reportlab.lib import colors
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.lib.pagesizes import A3, A2
-import pandas as pd
+import reportlab.pdfgen.canvas
+
+
+def calculate_max_df_width_height(df):
+    lines = []
+    for line_number in range(len(df)):
+        lines.append(len(''.join([str(element) for element in df.loc[line_number].values])))
+    max_width = max(lines)
+    max_height = df.shape[0] + 1
+    return max_width * 8.5, max_height * 200
 
 
 def export_df_to_pdf(df, filename):
-    elements = []
-    styles = getSampleStyleSheet()
-    doc = SimpleDocTemplate(filename + '.pdf', pagesizes=A3)
-    df_columns_names = pd.Series(['Название', 'Компания', 'Класс', 'Группа', 'Подгруппа', 'Средняя наработка на отказ',
-                                  'Средний срок сохраняемости', 'Средний ресурс (ч)', 'Среднее время восстановления (ч)',
-                                  'Дополнительная Информация', 'Дата добавления', 'Подтверждающая ссылка'])
-    df.columns = df_columns_names
-    df_data = [df.columns[:, ].values.astype(str).tolist()] + df.values.tolist()
+    width, height = calculate_max_df_width_height(df)
+
+    pagesize = (width, height)
+    canvas = reportlab.pdfgen.canvas.Canvas(filename + ".pdf", pagesize=pagesize)
+    # c.scale(0.24, 0.24) # Scale so that the image exactly fits the canvas.
+    # canvas.drawCentredString(width/2.0, height - 30, "Handbook: " + filename)
+
     pdfmetrics.registerFont(TTFont('DejaVuSerif-Italic', 'static/ReportLabFonts/DejaVuSerif-Italic.ttf'))
     ts = [('ALIGN', (1, 1), (-1, -1), 'CENTER'),
           ('FONT', (0, 0), (-1, -1), 'DejaVuSerif-Italic'),
@@ -24,6 +30,10 @@ def export_df_to_pdf(df, filename):
           ('BOX', (0, 0), (-1, 0), 0.8, colors.red),
           ('INNERGRID', (0, 0), (-1, 0), 0.25, colors.red)]
 
+    df_data = [df.columns[:, ].values.astype(str).tolist()] + df.values.tolist()  # values for pdf
     table = Table(df_data, style=ts)
-    elements.append(table)
-    doc.build(elements)
+    table_width, table_height = table.wrap(width, height)  # width and height of table
+    margin = int((width - table_width) / 2)
+    table.wrapOn(canvas, width, height)
+    table.drawOn(canvas, margin, height - table_height - 50)
+    canvas.save()
